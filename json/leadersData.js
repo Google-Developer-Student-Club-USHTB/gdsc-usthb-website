a13f0083b203f78fc3856a29c8d71452298e5ea9
MuhammedMiiRanna <merali.med@gmail.com>
2022-12-09 00:17:49 +0100

    ourTeam Section
    ourTeam section using pure CSS.
    leadersData contain the leaders informations.

---
+++ json/leadersData.js
@@ -0,0 +1,20 @@
+const OurTeam = [
+  {
+    name: "Brahimi Hazem",
+    image: "/assets/pic-rec3.png",
+    role: "Lead",
+    linkedin: "",
+    email: "",
+    github: "",
+    desc: "",
+},
+{
+    name: "Khemissi Maroua",
+    image: "/assets/pic-rec-2.png",
+    role: "Co-Lead",
+    linkedin: "",
+    email: "",
+    github: "",
+    desc: "",
+  },
+];
